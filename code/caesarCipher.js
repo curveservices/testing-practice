7ed3239b850c91329c46f4d1a414fd4950b2495b
Paul <p.rossiter83@gmail.noreply.github.com>
2023-11-06 18:03:52 +0000

    CaesarCipher completed

--- code/caesarCipher.js
+++ code/caesarCipher.js
@@ -1,9 +1,16 @@
-const caesarCipher = (str, key) => {
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    let result = '';
-    let string = str.toLowerCase();
-
-    for (let i = 0; i < string; i++) {
-        
-    }
+function shiftChar(char, shift) {
+   if (/[a-zA-Z]/.test(char)) {
+       const isUpperCase = char === char.toUpperCase();
+       const charCode = char.charCodeAt(0);
+       const base = isUpperCase ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0);
+       const shiftedCharCode = ((charCode - base + shift) % 26 + 26) % 26 + base;
+       return String.fromCharCode(shiftedCharCode);
+   }
+   return char;
 }
+
+function caesarCipher(str, shift) {
+    return str.split('').map((char) => shiftChar(char, shift)).join('');
+}
+
+module.exports = {caesarCipher, shiftChar};
--- test/caesarCipher.spec.js
+++ test/caesarCipher.spec.js
@@ -1,3 +1,24 @@
-const caesarCipher = require('../code/caesarCipher');
+const {caesarCipher, shiftChar} = require('../code/caesarCipher');
 
-test('')
+
+test('shiftChar shifts a letter correctly', () => {
+    expect(shiftChar('a', 1)).toBe('b');
+    expect(shiftChar('z', 1)).toBe('a');
+    expect(shiftChar('A', 3)).toBe('D');
+    expect(shiftChar('B', 3)).toBe('E');
+    expect(shiftChar('!', 1)).toBe('!');
+    expect(shiftChar('5', 1)).toBe('5');
+  });
+  
+  test('caesarCipher shifts a string correctly', () => {
+    expect(caesarCipher('abc', 1)).toBe('bcd');
+    expect(caesarCipher('zZ', 1)).toBe('aA');
+    expect(caesarCipher('Hello, World!', 5)).toBe('Mjqqt, Btwqi!');
+    expect(caesarCipher('XYZ', -1)).toBe('WXY');
+    expect(caesarCipher('AaZz 123 !', 7)).toBe('HhGg 123 !');
+    expect(caesarCipher('abcdefg', 26)).toBe('abcdefg'); // Test wrapping
+  });
+ 
+  
+  
+  
